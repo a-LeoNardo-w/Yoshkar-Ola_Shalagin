7e15fbe2eaf0f3fa38f426322e09412c3e05c08f
LeoNardo <99113210+a-LeoNardo-w@users.noreply.github.com>
2022-11-18 17:03:13 +0300

    add PyQT(updated)

--- PyQT/main.py
+++ PyQT/main.py
@@ -2,12 +2,31 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QPainter, QColor
+from random import randint
 
 
 class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.drawCircle(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def drawCircle(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(randint(0, self.width()), randint(0, self.height()), rad := randint(50, 150), rad)
 
 
 if __name__ == '__main__':
